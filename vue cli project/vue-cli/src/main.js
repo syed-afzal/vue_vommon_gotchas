b72bbece8a91f9fe15c61c061d75ba70788550ea
123afzal <sah.afzal@gmail.com>
2019-08-14 04:39:35 -0700

    first basic component in vue0cli

--- vue cli project/vue-cli/src/main.js
+++ vue cli project/vue-cli/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from './Home'
+
+//registring component globally
+Vue.component('app-server-status', Home);
 
 new Vue({
   el: '#app',
