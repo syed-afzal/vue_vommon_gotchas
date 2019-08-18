45732000a71bee0e707325e1127d9b64f44a0784
123afzal <sah.afzal@gmail.com>
2019-08-18 03:51:39 -0700

    communication between components init

---
+++ vue cli project/components-communication/src/components/User.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :name="name"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+          return {
+            name: 'Syed'
+          }
+        },
+        methods:{
+          changeName() {
+            this.name = 'Afzal'
+          }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
