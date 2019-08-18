294521ef52646c0ec6d4bd8553e2b597e836c28e
123afzal <sah.afzal@gmail.com>
2019-08-18 04:08:06 -0700

    adding custom event binding

--- vue cli project/components-communication/src/components/UserDetail.vue
+++ vue cli project/components-communication/src/components/UserDetail.vue
@@ -2,13 +2,27 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>My name: {{ name }}</p>
+        <p>My name: {{ myName }}</p>
+        <button @click="resetName">Reset name</button>
     </div>
 </template>
 
 <script>
   export default {
-    props: ['name']
+    // props: ['name']
+
+    //validation on props be like
+    props:{
+      myName:{
+        type: String
+      }
+    },
+    methods:{
+      resetName() {
+        this.myName = 'Syed';
+        this.$emit('nameWasChange', this.myName)
+      }
+    }
   }
 </script>
 
