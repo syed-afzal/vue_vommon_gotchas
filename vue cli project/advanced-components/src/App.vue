a99da596f0b0d799436812ad35b7322f57e26844
123afzal <sah.afzal@gmail.com>
2019-08-31 03:54:38 -0700

    slot basic usage, to render html inside component

---
+++ vue cli project/advanced-components/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2>The Quote</h2>
+          <p>A wonderful quote</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import Quote from './components/Quote'
+
+export default {
+  name: 'app',
+  components:{
+    appQuote: Quote
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
