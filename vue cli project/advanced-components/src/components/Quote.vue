8e0c58d0eea697d5375939f257e1a243718e8c53
123afzal <sah.afzal@gmail.com>
2019-08-31 03:56:00 -0700

    fixes

---
+++ vue cli project/advanced-components/src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+<!--      a keyword reserved by vue to output html inside that component-->
+      <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Quote"
+    }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    margin: 30px auto;
+    padding: 30px;
+    text-align: center;
+  }
+</style>
