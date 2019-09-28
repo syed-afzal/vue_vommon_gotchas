114f474f9dab1e8fc63c9566279fd27f4ea72387
123afzal <sah.afzal@gmail.com>
2019-09-28 12:32:04 -0700

    final touch

--- vue cli project/vue_forms_basic/src/App.vue
+++ vue cli project/vue_forms_basic/src/App.vue
@@ -122,13 +122,14 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -169,7 +170,13 @@
                 gender: 'Male',
                 priorities: ['High', 'Medium', 'Low'],
                 selectedPriority: 'Medium',
-                dataSwitch: true
+                dataSwitch: true,
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
             }
         },
         components:{
