7d82f21493550db6434bfb88fa3e039ff16c1e1f
123afzal <sah.afzal@gmail.com>
2019-09-29 07:35:56 -0700

    registring local directive

--- vue cli project/vue_directives/src/App.vue
+++ vue cli project/vue_directives/src/App.vue
@@ -12,6 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <p v-highlight:background.delayed="'blue'">Some Text</p>
+        <p v-local-highlight:background.delayed="'blue'">Some Text, color this too</p>
       </div>
     </div>
   </div>
@@ -23,6 +24,23 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  directives:{
+    'local-highlight': {
+      bind (el, binding, vnode) {
+        let delay = 0;
+        if(binding.modifiers['delayed']){
+          delay = 3000;
+        }
+        setTimeout(() => {
+          if(binding.arg === 'background'){
+            el.style.backgroundColor = binding.value
+          } else {
+            el.style.color = binding.value
+          }
+        }, delay)
+      }
     }
   }
 }
