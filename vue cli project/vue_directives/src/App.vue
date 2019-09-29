acd3121af31003f05e0ed835ff224bdf2ac4535c
123afzal <sah.afzal@gmail.com>
2019-09-29 07:46:49 -0700

    using multiple modifiers

--- vue cli project/vue_directives/src/App.vue
+++ vue cli project/vue_directives/src/App.vue
@@ -12,7 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <p v-highlight:background.delayed="'blue'">Some Text</p>
-        <p v-local-highlight:background.delayed="'blue'">Some Text, color this too</p>
+        <p v-local-highlight:background.delayed.blink="'blue'">Some Text, color this too</p>
       </div>
     </div>
   </div>
@@ -33,13 +33,29 @@ export default {
         if(binding.modifiers['delayed']){
           delay = 3000;
         }
-        setTimeout(() => {
-          if(binding.arg === 'background'){
-            el.style.backgroundColor = binding.value
-          } else {
-            el.style.color = binding.value
-          }
-        }, delay)
+        if(binding.modifiers['blink']) {
+          let mainColor = binding.value;
+          let secondColor = 'green';
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor === secondColor ? currentColor = mainColor : currentColor = secondColor;
+              if(binding.arg === 'background'){
+                el.style.backgroundColor = currentColor
+              } else {
+                el.style.color = currentColor
+              }
+            }, 1000)
+          }, delay)
+        } else {
+          setTimeout(() => {
+            if(binding.arg === 'background'){
+              el.style.backgroundColor = binding.value
+            } else {
+              el.style.color = binding.value
+            }
+          }, delay)
+        }
       }
     }
   }
