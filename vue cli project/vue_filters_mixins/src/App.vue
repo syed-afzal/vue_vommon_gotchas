e31195fd303ac28f13bacb97579e8494aac2d8ba
123afzal <sah.afzal@gmail.com>
2019-10-06 11:51:11 -0700

    search by case insensitive

--- vue cli project/vue_filters_mixins/src/App.vue
+++ vue cli project/vue_filters_mixins/src/App.vue
@@ -19,9 +19,9 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
       filteredText:'',
-      fruits: ['Apple', 'Mango', 'Banana', 'Orange']
+      msg: 'Welcome to Your Vue.js App',
+      fruits: ['Apple', 'Mango', 'Banana', 'Orange'],
     }
   },
   filters:{
@@ -30,8 +30,27 @@ export default {
     }
   },
   computed: {
+    RE(){
+      return new RegExp(this.filteredText, "i")
+    },
     filteredFruits() {
       return this.fruits.filter((element) => {
+        // if you want to match case-insensitive use these three ways
+
+        //One
+        //return element.toLowerCase().match(this.filteredText.toLowerCase())
+
+        //Two
+        // let RE = new RegExp(this.filteredText,"i");
+        // return element.match(this.RE)
+
+        // Three
+
+        // to make this 3rd way working we also placed regular expression in computed
+        // return element.match(this.RE)
+
+
+        // other-wise
         return element.match(this.filteredText)
       })
     }
