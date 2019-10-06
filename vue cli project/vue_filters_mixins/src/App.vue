03facf2860554c01b7be6792a476841da42ebc9a
123afzal <sah.afzal@gmail.com>
2019-10-06 10:26:23 -0700

    global filter

--- vue cli project/vue_filters_mixins/src/App.vue
+++ vue cli project/vue_filters_mixins/src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <p>{{ msg | toUpperCase }}</p>
+        <p>{{ msg | toUpperCase | to-lowercase }}</p>
       </div>
     </div>
   </div>
