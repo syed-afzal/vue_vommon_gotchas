22a1ccbf23f3b04c6a3b12473e7b548068163595
123afzal <sah.afzal@gmail.com>
2019-10-13 05:23:46 -0700

    split list code for mixins

--- vue cli project/vue_filters_mixins/src/List.vue
+++ vue cli project/vue_filters_mixins/src/List.vue
@@ -1,30 +1,20 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filter & Mixins</h1>
-        <p>{{ msg | toUpperCase | to-lowercase }}</p>
-        <hr>
-        <input type="text" v-model="filteredText">
-        <ul>
-          <li v-for="fruit in filteredFruits">{{ fruit }}</li>
-        </ul>
-        <hr>
-        <app-list></app-list>
-      </div>
-    </div>
+  <div>
+    <h1>Filter & Mixins</h1>
+    <hr>
+    <input type="text" v-model="filteredText">
+    <ul>
+      <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import List from './List'
-
   export default {
-    name: 'app',
+    name: 'List',
     data() {
       return {
         filteredText: '',
-        msg: 'Welcome to Your Vue.js App',
         fruits: ['Apple', 'Mango', 'Banana', 'Orange'],
       }
     },
@@ -58,38 +48,10 @@
           return element.match(this.filteredText)
         })
       }
-    },
-    components:{
-      appList: List
     }
   }
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
 
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  /*ul {*/
-  /*  list-style-type: none;*/
-  /*  padding: 0;*/
-  /*}*/
-
-  /*li {*/
-  /*  display: inline-block;*/
-  /*  margin: 0 10px;*/
-  /*}*/
-
-  a {
-    color: #42b983;
-  }
 </style>
