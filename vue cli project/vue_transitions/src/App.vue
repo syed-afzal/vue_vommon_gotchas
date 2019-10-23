2ed02dda00974c9e1b6499e9e3bc809363216152
123afzal <sah.afzal@gmail.com>
2019-10-23 12:12:39 -0700

    basic animation

--- vue cli project/vue_transitions/src/App.vue
+++ vue cli project/vue_transitions/src/App.vue
@@ -10,6 +10,9 @@
         <transition name="fade">
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
+        <transition name="slide">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -67,4 +70,38 @@
     transition: 1s;
     opacity: 0;
   }
+
+  .slide-enter {
+    /*transform: translateY(20px);*/
+    opacity: 0;
+  }
+  .slide-enter-active {
+    animation: slide-in 1s ease-out;
+    transition: opacity 1s;
+  }
+  .slide-leave {
+
+  }
+  .slide-leave-active {
+    animation: slide-out 1s ease-out;
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+    @keyframes slide-out {
+      from {
+        transform: translateY(0);
+      }
+      to {
+        transform: translateY(20px);
+      }
+    }
 </style>
