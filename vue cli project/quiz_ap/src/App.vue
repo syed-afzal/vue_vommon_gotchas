74cf8e0fe7d7f1430d80ad9e5b76a23277cc3fce
123afzal <sah.afzal@gmail.com>
2019-10-28 23:45:57 +0500

    animation touches

--- vue cli project/quiz_ap/src/App.vue
+++ vue cli project/quiz_ap/src/App.vue
@@ -8,7 +8,9 @@
     <hr>
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+        <transition name="flap" mode="out-in">
+          <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+        </transition>
       </div>
     </div>
   </div>
@@ -41,3 +43,34 @@
   }
 </script>
 
+<style>
+  .flap-enter {
+
+  }
+  .flap-enter-active {
+    animation: flap-in 0.5s ease-out forwards;
+  }
+  .flap-leave {
+
+  }
+  .flap-leave-active {
+    animation: flap-out 0.5s ease-out forwards;
+  }
+
+  @keyframes flap-out {
+    from {
+      transform: rotateY(0deg);
+    }
+    to {
+      transform: rotateY(90deg);
+    }
+  }
+  @keyframes flap-in {
+    from {
+      transform: rotateY(90deg);
+    }
+    to {
+      transform: rotateY(0deg);
+    }
+  }
+</style>
